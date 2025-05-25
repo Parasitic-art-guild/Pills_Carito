ddd2d9ab04a76cc77db1545db1fa7a1715ff5690
Parasitic-art-guild <127715174+Parasitic-art-guild@users.noreply.github.com>
2025-05-25 09:02:59 -0500

    Add files via upload
    20 may 2025 Primero activa el motor y luego muestra presencia de tarro.
    25 may 2025 Se activa motor primero porque la condición inicial es ver si confirmacionlectura es true, como es una const int para designar el pin de la tarjeta la detecta como true, para corregir se reemplaza por un bitconfirmacionlectura.

--- Pills_Carito_Task_Union.ino
+++ Pills_Carito_Task_Union.ino
@@ -7,6 +7,7 @@ unsigned long tiempoNoSensorTarroTarea1 = 5000;
 unsigned long tiempoInicioTarea11 = millis();
 unsigned long tiempoInicioTarea12 = millis();
 const int confirmacionlectura = 7;
+bool bitconfirmacionlectura = false;
 
 //freno izquierdo
 const int frenoizquierdo = 5;
@@ -43,11 +44,13 @@ const int prueba = 3;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(1200);
   pinMode(sensortarro, INPUT);
   pinMode(frenoizquierdo, OUTPUT);  //relacionado con cilindro izquierdo
   pinMode(frenoderecho, OUTPUT);
   pinMode(prueba, INPUT);
   pinMode(confirmacionlectura, OUTPUT);
+  digitalWrite(frenoderecho, HIGH);
   // pinMode(iniciosecuenciallenado, INPUT);
   // digitalRead(iniciosecuenciallenado);
   pinMode(motorPin, OUTPUT);  // Configurar el pin del motor como salida
@@ -59,6 +62,7 @@ void tarea10() {
   if (digitalRead(sensortarro) && !bitSiSensorTarroTarea11 && millis() - tiempoInicioTarea11 >= tiempoSiSensorTarroTarea1) {
     //Se cumple si sensor ve tarro por tiempoSi y si bitSiSensor es false
     Serial.println("Presencia de tarro 11");
+    bitconfirmacionlectura = true;
     digitalWrite(confirmacionlectura, HIGH);  // si sensor ve por tiempoSi entonces activa LED,
                                               // este codigo es representación visual de lectura
     bitSiSensorTarroTarea11 = true;           // es verdadera para que condicional no se repita y se active el condicional que
@@ -71,6 +75,7 @@ void tarea10() {
     }
   }
   if (bitSiSensorTarroTarea11 && !bitSiSensorTarroTarea12 && !digitalRead(sensortarro) && millis() - tiempoInicioTarea12 >= tiempoNoSensorTarroTarea1) {
+    bitconfirmacionlectura = false;
     Serial.println("Se perdio tarro");
     digitalWrite(confirmacionlectura, LOW);  // si sensor NO ve por tiempoNO entonces desactiva LED,
     bitSiSensorTarroTarea12 = true;          //para inhabilitar esta condicional o tarea.
@@ -85,14 +90,13 @@ void tarea10() {
 }
 
 void tarea20() {
-  if (confirmacionlectura /*digitalRead(iniciosecuenciallenado)*/) {
+  if (bitconfirmacionlectura /*digitalRead(iniciosecuenciallenado)*/) {
     // Activar el motor después de un tiempo de espera
     if (!finsecuenciallenado && !motorActivo && millis() - tiempoInicio >= tiempoInicioMotor) {
       digitalWrite(motorPin, HIGH);
       motorActivo = true;
       Serial.println("Motor activado.");
     }
-
     // Detectar la leva y preparar el paro del motor
     if (!finsecuenciallenado && motorActivo && !esperandoParo && digitalRead(sensorPin) == HIGH) {
       Serial.println("Leva detectada. Preparando para detener el motor...");
@@ -103,8 +107,6 @@ void tarea20() {
     if (!finsecuenciallenado && esperandoParo && millis() - tiempoDeteccionLeva >= tiempoParadaMotor) {
       digitalWrite(motorPin, LOW);  // Apagar el motor
       motorActivo = false;          // Resetear estado del motor
-
-
       esperandoParo = false;    // Resetear estado de espera
       tiempoInicio = millis();  // Reiniciar el tiempo inicial
       Serial.println("Motor detenido.");
@@ -148,6 +150,7 @@ void tarea30() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Serial.println("loop");
   tarea10();  //Activar freno derecho, leer sensor de tarro y activar sensor freno izquierdo mientras ve el tarro. En caso
               //que no vea tarro, entonces desactiva freno izquierdo.
   tarea20();  //secuencia de llenado, se activa plato tolva con delay y se para con delay cuando el sensor ya observo la leva.
